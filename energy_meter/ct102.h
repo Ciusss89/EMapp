f2ae3df833c3a32b0de54d0b11bbb127d08aafef
Giuseppe Tipaldi <giuseppe.tipaldi@tanaza.com>
2019-10-26 12:10:05 +0200

    energy_meter: add current transformer (ct) ta1020

---
+++ energy_meter/ct102.h
@@ -0,0 +1,14 @@
+
+#ifndef CT1020_H
+#define CT1020_H
+
+/* Current Transformers (CT) sensor YHDC TA1020 */
+
+#define ABS_MAX_INPUT	20U	/* Assume a range  0-20A max */
+#define CT_MAX_OUPUT	20U	/* Max rated ouput in mA */
+#define CT_RATIO	1000U	/* Turns ratio */
+#define CT_LOAD		50U	/* Max Sampling resistance in Ω */
+#define CT_PHASE_SHIFT	12U	/* Phase shitf 12.3' */
+
+#endif
+
--- energy_meter/em.h
+++ energy_meter/em.h
@@ -3,9 +3,21 @@
 #define EM_H
 
 #include <stdlib.h>
+#include "ct102.h"
 
+#define MAX_CURRENT	12U	/* Maxinum Measurable Current */
+#define K		1000
+
+/*
+ * I prefer don't have a rail-to-rail waveform, such expedient should
+ * reduce the no-linearity contribs. I instead to set 3.3V I'll use 3V
+ */
+#define ANALOG_IN_VPP	3U
+
+#ifndef M_SQRT2
+#define M_SQRT2		1.41421356237309504880
+#endif
 
 int em_handler(int argc, char *argv[]);
 
 #endif
-
